04f728879034fe71990374b6e704aab5860cc7c4
Ahmed Ayman <a.ayman6000@gmail.com>
2021-02-15 10:27:21 +0200

    Update Add-Two-Numbers.cs

--- Algorithms/Add-Two-Numbers.cs
+++ Algorithms/Add-Two-Numbers.cs
@@ -15,10 +15,12 @@
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * public class ListNode 
+ * {
  *     public int val;
  *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
+ *     public ListNode(int val=0, ListNode next=null) 
+ *     {
  *         this.val = val;
  *         this.next = next;
  *     }
