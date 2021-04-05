fbdd4f0ef5a9b299a872e11b718ccd4f4061b56f
Ahmed Ayman <a.ayman6000@gmail.com>
2021-04-05 23:57:14 +0200

    Create single-number.cs

---
+++ Algorithms/single-number.cs
@@ -0,0 +1,31 @@
+/**
+   Problem Name : Single Number
+   Problem URL : https://leetcode.com/problems/single-number/
+   Description :
+    Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+   Difficulty : Easy
+   Language : C#
+   Category : Algorithms
+*/
+public class Solution 
+{
+    public int SingleNumber(int[] nums) 
+    {
+        Dictionary<int, int> reps = new Dictionary<int, int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(reps.ContainsKey(nums[i]))
+                reps[nums[i]]++;
+            else
+                reps[nums[i]] = 1;
+        }
+        foreach(KeyValuePair<int, int> num in reps)
+        {
+            if(num.Value == 1)
+                return num.Key;
+        }
+        
+        return 0;
+    }
+    
+}
